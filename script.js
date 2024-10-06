9fd7f81ff3eb1c41f6e30bb59d6fa6d325651260
Mar <111702294+MariamGutierrez@users.noreply.github.com>
2024-10-06 02:07:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ let imageIndex = 0; // Índice de la imagen actual
 const images = ['imagen1.jpg', 'imagen2.jpg', 'imagen3.jpg']; // Lista de imágenes
 
 function changeImage() {
-    const widgetImage = document.getElementById('widgetImage');
-
     // Cambia la imagen al siguiente índice en la lista
     imageIndex = (imageIndex + 1) % images.length;
-    widgetImage.src = images[imageIndex];
-    
+
     // Actualiza la base de datos Firebase con el nuevo índice de la imagen
     firebase.database().ref('NOTIONBUTTON').set({
         imageIndex: imageIndex
